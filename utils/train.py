8ae66bdac0223f8ed2512cd7358a9e543ff5d3ab
williamjohn972 <williamjohn972@gmail.com>
2025-10-26 15:53:30 +0800

    Major Changes

--- utils/train.py
+++ utils/train.py
@@ -23,6 +23,7 @@ def train_loop(model: nn.Module,
     
     train_losses = []
     y_pred_train = []
+    y_pred_logits_train = []
     y_true_train = []
 
     for batch in tqdm(train_dataloader, desc="Training", leave=False):
@@ -32,23 +33,25 @@ def train_loop(model: nn.Module,
 
         optimizer.zero_grad()
 
-        y_pred = model(x)
-        loss = loss_fn(y_pred, y)
+        y_pred_logits = model(x)
+        loss = loss_fn(y_pred_logits, y)
 
         loss.backward()
         optimizer.step()
 
         train_losses.append(loss.item())
 
-        y_pred_train.append(y_pred)
+        y_pred_train.append(y_pred_logits.argmax(dim=1))
         y_true_train.append(y)
+        y_pred_logits_train.append(y_pred_logits)
 
         # if (batch_index + 1) % 100 == 0:  # every 100 batches
         #     print(f"Batch [{batch_index + 1}/{len(trainloader)}], Loss: {loss.item():.4f}")
     return {
         "losses": train_losses,
         "y_pred": y_pred_train,
-        "y_true": y_true_train
+        "y_true": y_true_train,
+        "y_pred_logits": y_pred_logits_train
     }
         
 def val_loop(model: nn.Module,
@@ -62,24 +65,27 @@ def val_loop(model: nn.Module,
         val_losses = []
         y_pred_val = []
         y_true_val = []
+        y_pred_logits_val = []
 
         for batch in tqdm(val_dataloader, desc="Validation", leave=False):
             x = batch["x"].to(device)
             y = batch["y"].to(device)
 
-            y_pred = model(x)
-            loss = loss_fn(y_pred, y)
+            y_pred_logits = model(x)
+            loss = loss_fn(y_pred_logits, y)
 
             val_losses.append(loss.item())
 
-            y_pred_val.append(y_pred)
+            y_pred_val.append(y_pred_logits.argmax(dim=1))
             y_true_val.append(y)
+            y_pred_logits_val.append(y_pred_logits)
 
     
     return {
         "losses": val_losses,
         "y_pred": y_pred_val,
-        "y_true": y_true_val
+        "y_true": y_true_val,
+        "y_pred_logits": y_pred_logits_val
     }
         
 def test_loop(model: nn.Module,
@@ -91,24 +97,27 @@ def test_loop(model: nn.Module,
 
         y_pred_test = []
         y_true_test = []
+        y_pred_logits_test = []
 
         for batch in tqdm(test_dataloader, desc="Testing", leave=False):
             x = batch["x"].to(device)
             y = batch["y"].to(device)
 
-            y_pred = model(x)
+            y_pred_logits = model(x)
 
-            y_pred_test.append(y_pred)
+            y_pred_test.append(y_pred_logits.argmax(dim=1))
             y_true_test.append(y)
+            y_pred_logits_test.append(y_pred_logits)
 
 
         final_y_pred_test = torch.cat(y_pred_test).cpu().numpy()
         final_y_true_test = torch.cat(y_true_test).cpu().numpy()
-
+        final_y_pred_logits = torch.cat(y_pred_logits_test).cpu().numpy()
     
     return {
         "y_pred": final_y_pred_test,
-        "y_true": final_y_true_test
+        "y_true": final_y_true_test,
+        "y_pred_logits": final_y_pred_logits
     }
         
 # MAIN
@@ -130,6 +139,7 @@ def train_and_eval_model(
     history = {
         "train_loss": [], "val_loss": [],
         "train_pred": [], "val_pred": [],
+        "train_pred_logits":[], "val_pred_logits": [],
         "train_true": [], "val_true": []
     }
 
@@ -145,6 +155,8 @@ def train_and_eval_model(
         train_loss = avg_loss(train_data["losses"])
         train_pred = torch.cat(train_data["y_pred"])
         train_true = torch.cat(train_data["y_true"])
+        train_pred_logits = torch.cat(train_data["y_pred_logits"])
+
 
         if debug:
             print(f"Classes Predicted: {train_pred.unique()}")
@@ -158,6 +170,7 @@ def train_and_eval_model(
         val_loss = avg_loss(val_data["losses"])
         val_pred = torch.cat(val_data["y_pred"])
         val_true = torch.cat(val_data["y_true"])
+        val_pred_logits = torch.cat(val_data["y_pred_logits"])
         
         # Early Stopper 
         if early_stopper is not None:
@@ -171,10 +184,15 @@ def train_and_eval_model(
         # store metrics in history
         history["train_loss"].append(train_loss)
         history["val_loss"].append(val_loss)
+
         history["train_pred"].append(train_pred.cpu())
         history["train_true"].append(train_true.cpu())
+
         history["val_pred"].append(val_pred.cpu())
         history["val_true"].append(val_true.cpu())
+
+        history["train_pred_logits"].append(train_pred_logits.cpu())
+        history["val_pred_logits"].append(val_pred_logits.cpu())
 
         if verbose:
             # Print Logs
@@ -198,34 +216,34 @@ def train_and_eval_model(
 
 
 # PLOTTING
-def plot_training_history(history: dict, best_epoch):
+# def plot_training_history(history: dict, best_epoch):
 
-    TRAIN_COLOUR = "blue"
-    VAL_COLOUR = "red"
-    AXVLINE_COLOUR = "green"
-    AXVLINE_STYLE = "--"
+#     TRAIN_COLOUR = "blue"
+#     VAL_COLOUR = "red"
+#     AXVLINE_COLOUR = "green"
+#     AXVLINE_STYLE = "--"
 
-    epochs = np.arange(1, len(history['train_loss']) + 1)
+#     epochs = np.arange(1, len(history['train_loss']) + 1)
 
-    plt.figure(figsize=(15,5))
+#     plt.figure(figsize=(15,5))
 
-    # Plot loss curves
-    plt.subplot(1,2,1)
-    plt.plot(epochs, history['train_loss'], label="Train Loss", color=TRAIN_COLOUR)
-    plt.plot(epochs, history['val_loss'], label="Validation Loss", color=VAL_COLOUR)
-    plt.axvline(x=best_epoch, color=AXVLINE_COLOUR, linestyle=AXVLINE_STYLE, label=f"Best Loss ({best_epoch})")
+#     # Plot loss curves
+#     plt.subplot(1,2,1)
+#     plt.plot(epochs, history['train_loss'], label="Train Loss", color=TRAIN_COLOUR)
+#     plt.plot(epochs, history['val_loss'], label="Validation Loss", color=VAL_COLOUR)
+#     plt.axvline(x=best_epoch, color=AXVLINE_COLOUR, linestyle=AXVLINE_STYLE, label=f"Best Loss ({best_epoch})")
 
-    plt.title('Loss Curves')
-    plt.xlabel('Epoch')
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.grid(True)
+#     plt.title('Loss Curves')
+#     plt.xlabel('Epoch')
+#     plt.ylabel("Loss")
+#     plt.legend()
+#     plt.grid(True)
 
-    # Plot acc curves
-    plt.subplot(1,2,2)
-    plt.plot(epochs, history["train_acc"], color=TRAIN_COLOUR)
-    plt.plot(epochs, history["val_acc"], color=VAL_COLOUR)
-    plt.axvline(x=best_epoch, color=AXVLINE_COLOUR, linestyle=AXVLINE_STYLE, label=f"Best Acc ({best_epoch})")
+#     # Plot acc curves
+#     plt.subplot(1,2,2)
+#     plt.plot(epochs, history["train_acc"], color=TRAIN_COLOUR)
+#     plt.plot(epochs, history["val_acc"], color=VAL_COLOUR)
+#     plt.axvline(x=best_epoch, color=AXVLINE_COLOUR, linestyle=AXVLINE_STYLE, label=f"Best Acc ({best_epoch})")
 
     
 
